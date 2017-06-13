ad033770086ab4337cfc61db317a5af73d58f2f2
Steve <chengquan.wang@ele.me>
2017-06-13 10:53:44 +0800

    init the repo

---
+++ Foundation+/Int+.swift
@@ -0,0 +1,37 @@
+//
+//  Int.swift
+//  Elegant
+//
+//  Created by Steve on 2017/5/18.
+//  Copyright © 2017年 KingCQ. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    ///  Returns a string representing the Int.
+    var toString: String {
+        return String(self)
+    }
+}
+
+extension Int8 {
+    ///  Returns a string representing the Int.
+    var toString: String {
+        return String(self)
+    }
+}
+
+extension Int16 {
+    ///  Returns a string representing the Int.
+    var toString: String {
+        return String(self)
+    }
+}
+
+extension Int64 {
+    ///  Returns a string representing the Int.
+    var toString: String {
+        return String(self)
+    }
+}
