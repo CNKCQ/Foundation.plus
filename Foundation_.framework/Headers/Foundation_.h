36fb999f9749a3aab816aa8993ec7155d6922a71
Steve <chengquan.wang@ele.me>
2017-11-21 16:59:03 +0800

    update the spec file

---
+++ Foundation_.framework/Headers/Foundation_.h
@@ -0,0 +1,19 @@
+//
+//  Foundation_.h
+//  Foundation+
+//
+//  Created by Steve on 21/11/2017.
+//  Copyright © 2017 KingCQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Foundation_.
+FOUNDATION_EXPORT double Foundation_VersionNumber;
+
+//! Project version string for Foundation_.
+FOUNDATION_EXPORT const unsigned char Foundation_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Foundation_/PublicHeader.h>
+
+
